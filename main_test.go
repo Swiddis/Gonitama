1569db6935649586921ecc305aab1ac09fd5471d
Simeon Widdis <sawiddis@gmail.com>
2022-07-17 10:51:57 -0600

    Separate playout and search benchmarks

--- main_test.go
+++ main_test.go
@@ -5,10 +5,11 @@ import (
 	"os"
 	"testing"
 
+	"git.sr.ht/~bonbon/gmcts"
 	"github.com/Swiddis/gonitama/onitama"
 )
 
-func BenchmarkSearch(b *testing.B) {
+func BenchmarkPlayouts(b *testing.B) {
 	cardData, err := os.ReadFile("./data/cards.json")
 	if err != nil {
 		log.Fatal("Unable to read card json: " + err.Error())
@@ -23,6 +24,30 @@ func BenchmarkSearch(b *testing.B) {
 
 		if board.IsTerminal() {
 			board = onitama.InitialBoard()
+		}
+	}
+}
+
+func BenchmarkSearch(b *testing.B) {
+	cardData, err := os.ReadFile("./data/cards.json")
+	if err != nil {
+		log.Fatal("Unable to read card json: " + err.Error())
+	}
+	onitama.LoadCards(cardData)
+
+	board := onitama.InitialBoard()
+	mcts := gmcts.NewMCTS(board)
+	tree := mcts.SpawnTree()
+	for n := 0; n < b.N; n++ {
+		tree.SearchRounds(1)
+		if n%1000 == 0 {
+			mcts.AddTree(tree)
+			next, _ := board.ApplyAction(mcts.BestAction())
+			board = next.(onitama.BitBoard)
+			if board.IsTerminal() {
+				board = onitama.InitialBoard()
+				mcts = gmcts.NewMCTS(board)
+			}
 		}
 	}
 }
